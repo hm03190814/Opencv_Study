e18e2fb9ded8f1a17e00f39e147e36333bc06530
hm03190814 <154034852+hm03190814@users.noreply.github.com>
2024-05-15 13:17:23 +0800

    Delete ssh.sh

--- ssh.sh
+++
@@ -1,4 +0,0 @@
-sudo  rm -rf .git && git init && git add .
-git commit -m "2024_03_08"
-git remote add opencv_my_study git@github.com:hm03190814/opencv_my_study.git
-git push opencv_my_study  master
