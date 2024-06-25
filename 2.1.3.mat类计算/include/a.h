b51b8d89ea275d3a4ae6817312e0b230292dbb34
hm03190814 <154034852+hm03190814@users.noreply.github.com>
2024-06-25 15:55:30 +0800

    Add files via upload

---
+++ 2.1.3.mat类计算/include/a.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include<opencv4/opencv2/core/core.hpp>
+#include<opencv4/opencv2/highgui.hpp>
+#include<opencv4/opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+class CV_Class{
+public:
+    void func();
+};
